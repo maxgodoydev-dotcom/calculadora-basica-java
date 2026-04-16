236ea39cf1126b1a076070b17c1d1dab53351994
Max Godoy <maxgodoy.dev@gmail.com>
2026-04-16 20:49:10 -0300

    Calculadora-Basica_java

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("=== Calculadora Básica ===");
+        System.out.println("Bem-vindos à minha calculadora básica!");
+    }
+}
---
+++ src/Calculadora.java
@@ -0,0 +1,22 @@
+public class Calculadora {
+
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public double dividir(int a, int b) {
+        if (b == 0) {
+            System.out.println("Erro fatal!: Não pode dividir zero!");
+            return 0;
+        }
+        return (double) a / b;
+    }
+}
